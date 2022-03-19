091f503c0b8ae928d1feaa6b81b30a203b4c82b7
Szymon Szeliga <szeliga.szymon@gmail.com>
2022-03-19 18:54:14 +0100

    Add kitty config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/.config/dotfiles/extras/git ~/.config/dotfiles/extras/zsh
+mkdir -p ~/.config/dotfiles/extras/git ~/.config/dotfiles/extras/zsh ~/.config/lvim ~/.config/kitty
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -sf ~/.config/dotfiles/hammerspoon ~/.hammerspoon
 fi
@@ -10,3 +10,4 @@ ln -sf ~/.config/dotfiles/zshrc ~/.zshrc
 ln -sf ~/.config/dotfiles/gemrc ~/.gemrc
 ln -sf ~/.config/dotfiles/tigrc ~/.tigrc
 ln -sf ~/.config/dotfiles/lvim.lua ~/.config/lvim/config.lua
+ln -sf ~/.config/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
